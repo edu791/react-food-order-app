ecc1cb4b965d8a82080860dba07b65e32f256541
Eduardo <eduardo@Eduardos-MacBook-Air.local>
2021-05-26 23:09:00 -0400

    Commit 4

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,29 @@
+import styles from "./Modal.module.css";
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+function Backdrop() {
+  return <div className={styles.backdrop}></div>;
+}
+
+function ModalOverlay(props) {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>{props.children}</div>
+    </div>
+  );
+}
+
+const portalElement = document.getElementById("overlays");
+
+export default function Modal(props) {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+}
