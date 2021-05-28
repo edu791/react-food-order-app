aa1995558db661f84cbe4ef7092984bfa6d650da
Eduardo <eduardo@Eduardos-MacBook-Air.local>
2021-05-28 01:11:58 -0400

    Commit 9

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,30 @@
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context'
 
 export default function HeaderCartButton(props) {
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const numberOfCartItems = items.length;
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
 
-  const numberOfCartItems = cartCtx.items.length;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
